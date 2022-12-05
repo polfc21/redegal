8d80f0aab9c5c7f239bf6665bdfcff72a9e464ff
pol <pol@pol.home>
2022-12-05 16:01:09 +0100

    Initial Redegal application

---
+++ src/main/java/com/redegal/RedegalApplication.java
@@ -0,0 +1,13 @@
+package com.redegal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedegalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedegalApplication.class, args);
+    }
+
+}
